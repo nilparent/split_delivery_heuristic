1a3740018869b9c9e9a9ea8bd2d3708e0c68f78c
nilparent <nil.parent@orange.fr>
2022-03-10 00:23:01 +0100

    distance+text

--- instance_generator.cpp
+++ instance_generator.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include "instance_generator.h"
 #include <cmath>
+#include <cstdlib>
 using namespace std;
 #include <Imagine/Graphics.h>
 using namespace Imagine;
@@ -15,6 +16,8 @@ Instance :: Instance(int numberofclient,int Xmap,int Ymap,int Maxdemand){
     xmap=Xmap;
     ymap = Ymap;
     listclient = new Client[nbclient];
+    srand ( (unsigned int)time(0) );
+
     for(int i=0;i<nbclient;i++){
         listclient[i].demand = rand()%(maxdemand-1) + 1;//a client cannot have a demand null
         listclient[i].x = rand()%xmap;
@@ -27,4 +30,23 @@ void Instance :: displayinstance(){
         fillCircle(listclient[i].x,listclient[i].y,5*listclient[i].demand,BLACK);
     }
     endGraphics();
+}
+
+double distance(Client a, Client b) {
+    double xa=a.x;
+    double xb=b.x;
+    double ya=a.y;
+    double yb=b.y;
+    double dist = sqrt((xa-xb)*(xa-xb)+(ya-yb)*(ya-yb));
+    return dist;
+}
+
+void writeFile (std::string directory, std::string to_write)
+{
+  ofstream myfile;
+  myfile.open(directory);
+  myfile << to_write +"\n";
+
+  myfile.close();
+  return ;
 }
